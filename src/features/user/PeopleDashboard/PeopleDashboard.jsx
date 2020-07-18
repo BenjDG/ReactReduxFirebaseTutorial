6b8fd83f959cd1c33a0da29503255c537b96e9a1
Ben Galloway <bdgalloway85@gmail.com>
2020-07-18 15:50:31 -0500

    add extra pages for react routes

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PeopleDashboard = () => {
+    return (
+        <div>
+            <h1>People dashboard</h1>
+        </div>
+    )
+}
+
+default export PeopleDashboard;
